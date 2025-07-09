caa4ebefbd9c32f4b6dfccead59a111ee014691e
kodo <kodo@linux.do>
2025-07-09 10:01:03 +0800

    no message

--- aliases.sh
+++ aliases.sh
@@ -2,6 +2,7 @@
 alias dnginx='docker exec -it nginx /bin/sh'
 alias dphp82='docker exec -it php82 /bin/bash'
 alias dphp74='docker exec -it php74 /bin/bash'
+alias dphp56='docker exec -it php56 /bin/bash'
 alias dmysql57='docker exec -it mysql57 /bin/bash'
 alias dredis='docker exec -it redis /bin/sh'
 alias dnvm='docker exec -it nvm /bin/bash'
